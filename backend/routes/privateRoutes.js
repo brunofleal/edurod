522762f601c3253c27662dfd7996ab5c6e674323
Bruno Leal <brunofilizolaleal@gmail.com>
2025-09-29 17:51:27 -0300

    -

--- backend/routes/privateRoutes.js
+++ backend/routes/privateRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const authenticateUser = require('./verifyToken');
 
 // using authenticateUser as a midddleware for validation
-router.get('/', authenticateUser,(req, res) => {
+router.get('/', authenticateUser, (req, res) => {
     res.send(req.user)
 })
 
